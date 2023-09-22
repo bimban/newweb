b781a6559be39f5e3ab000a79911ffcff593ff35
bimban <145782768+bimban@users.noreply.github.com>
2023-09-22 23:17:12 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+
+
+
+  var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
